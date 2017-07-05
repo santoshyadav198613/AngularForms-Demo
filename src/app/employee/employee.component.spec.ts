ff55dae8466dc802b4b911520753f63b6875c0ef
Santosh Yadav <santosh.yadav198613@gmail.com>
2017-07-05 21:41:29 +0530

    unit testing with forms

--- src/app/employee/employee.component.spec.ts
+++ src/app/employee/employee.component.spec.ts
@@ -1,25 +1,44 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EmployeeComponent } from './employee.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
 
 describe('EmployeeComponent', () => {
   let component: EmployeeComponent;
   let fixture: ComponentFixture<EmployeeComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EmployeeComponent ]
+      imports: [ReactiveFormsModule, FormsModule],
+      declarations: [EmployeeComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EmployeeComponent);
     component = fixture.componentInstance;
+    component.ngOnInit();
     fixture.detectChanges();
   });
 
   it('should be created', () => {
+    console.log(component);
     expect(component).toBeTruthy();
   });
+
+  it('form invalid when empty', () => {
+    expect(component.employeeForm.valid).toBeFalsy();
+  });
+
+  it('form invalid when filled', () => {
+    component.employeeForm.controls['name'].setValue('Santosh');
+    expect(component.employeeForm.valid).toBeFalsy();
+  });
+
 });
